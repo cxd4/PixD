788c522cd32b45e2ec25a6a4d0da6969b1f13684
unknown <rj150@hotmail.com>
2015-08-28 12:05:49 -0400

    Squeeze the Win64 build size down to 10 KB.

--- make_wdk.cmd
+++ make_wdk.cmd
@@ -8,11 +8,11 @@ set MSVC=%DDK%\bin\x86\%target%
 set incl=/I"%DDK%\inc\crt" /I"%DDK%\inc\api"
 set libs=/LIBPATH:"%DDK%\lib\crt\%target%" /LIBPATH:"%DDK%\lib\wnet\%target%"
 
-set LINK_FLAGS=/OUT:PixD.exe %libs% kernel32.lib opengl32.lib glut32.lib
+set LINK_FLAGS=/MERGE:.rdata=.text %libs% kernel32.lib opengl32.lib glut32.lib
 set src=%CD%
 set C_FLAGS=/W4 /GL /O1 /Os /Oi /Ob1 /GS- /TC /Fa
 set files=%src%\main.c %src%\state.c %src%\image.c
 
-%MSVC%\cl.exe /MD %incl% %C_FLAGS% %files% /link %LINK_FLAGS%
+%MSVC%\cl.exe /MD %incl% %C_FLAGS% %files% /link /OUT:PixD.exe %LINK_FLAGS%
 
 pause
